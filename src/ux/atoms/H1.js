23b1bc9f97d7cc32db3dafeb92674eba2e987bd9
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-05-30 01:11:07 -0700

    update file system, remove src in src

---
+++ src/ux/atoms/H1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../../const/colors';
+
+const H1 = styled.h1`
+    color: ${colors.fourth}
+`;
+
+export default H1;

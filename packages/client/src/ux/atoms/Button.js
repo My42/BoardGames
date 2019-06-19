8387f82e4a344b848a81d6ac4ff464ae968a002d
Vincent Mesquita <vincent.mesquita@epitech.eu>
2019-06-18 20:19:32 -0700

    update Button : props

--- packages/client/src/ux/atoms/Button.js
+++ packages/client/src/ux/atoms/Button.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Button as Btn } from 'antd';
+import Proptypes from 'prop-types';
 import colors from '../../const/colors';
 
-const style = {
+const defaultStyle = {
   backgroundColor: colors.third,
   borderColor: colors.secondary,
   color: colors.fourth,
 };
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ children, ...props }) => (
-  <Btn block style={style} {...props}>{children}</Btn>
+const Button = ({ children, style, ...props }) => (
+  <Btn block style={{ ...defaultStyle, ...style }} {...props}>{children}</Btn>
 );
+
+Button.propTypes = { style: Proptypes.shape({ }) };
+
+Button.defaultProps = { style: { } };
 
 export default Button;
